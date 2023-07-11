c16dd0cec575154637e7ede3b9aea8635b0ea7f9
Monika <monidavies@gmail.com>
2023-07-11 16:24:18 -0700

    Makes get request, updates date formatter

--- src/DisplayContainer.js
+++ src/DisplayContainer.js
@@ -1,8 +1,37 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import getMonday from './dateFormatter2';
+import axios from 'axios';
 
 function DisplayContainer() {
+  const[week, setWeek] = useState(getMonday(new Date()));
+  const[user, setUser] = useState('lovebug@veggies.com');
+  const[plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    getPlants();
+  }, []);
+
+
+  async function getPlants() {
+    try {
+      const url = `${process.env.REACT_APP_SERVER}/week?date=${week}`;
+      const res = await axios.get(url);
+      console.log(url);
+      console.log(`res.data: ${JSON.stringify(res.data)}`);
+      setPlants(res.data[0].plants);
+    } catch(error){
+      console.log(error);
+    }
+  }
+
   return(
-    <div className="display-container"></div>
+    <div className="display-container">
+      {plants.map((plant, idx) => 
+        <div idx={idx}>{plant}</div>
+      )}
+
+    </div>
   )
 }
 
---
+++ src/dateFormatter2.js
@@ -0,0 +1,12 @@
+export default function getMonday(date) {
+  const millisecondsToSubtract = ((date.getDay() + 6) % 7) * 24 * 60 * 60 * 1000;
+  const newDate = new Date(date.getTime() - millisecondsToSubtract);
+
+  const year = newDate.getFullYear();
+  const month = newDate.getMonth() + 1;
+  const day = newDate.getDate();
+
+  console.log(month + '-' + day + '-' + year);
+
+  return month + '-' + day + '-' + year;
+}
