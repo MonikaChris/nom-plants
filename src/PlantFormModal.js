1685d935dcdb093ed2e9e7f6f9a1de803ae4ea53
Monika <monidavies@gmail.com>
2023-07-28 20:53:50 -0700

    Styles add plant modal

--- src/PlantFormModal.js
+++ src/PlantFormModal.js
@@ -17,20 +17,20 @@ export default function PlantFormModal({setPlantModal, addPlant}) {
     <div className="plant-form-modal" >
       <h2>Nom'd a Plant?</h2>
       <form className="plant-form" onSubmit={handleSubmit}>
-        <div>
-        <label htmlFor="plant-type">Plant:</label>
-        <input 
-          id="plant-type"
-          type="text"
-          value={plantType}
-          onChange={e => setPlantType(e.target.value)}  
-        />
-        </div>
+          <div id='add-plant-text'>
+            <label htmlFor="plant-type">Plant:</label>
+            <input 
+              id="plant-type"
+              type="text"
+              value={plantType}
+              onChange={e => setPlantType(e.target.value)}  
+            />
+          </div>
 
-        <div>
-        <button className="plant-form-button" type="submit">Add Plant</button>
-        <button className="plant-form-button" onClick={() => setPlantModal(false)}>Cancel</button>
-        </div>
+          <div>
+            <button className="plant-form-button" type="submit">Add Plant</button>
+            <button className="plant-form-button" onClick={() => setPlantModal(false)}>Cancel</button>
+          </div>
       </form>
     </div>
   )
