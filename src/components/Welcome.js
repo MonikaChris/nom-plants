9ef91de5501642a40eacb00a0eaf67f2d0c14b9f
Monika <monidavies@gmail.com>
2023-09-30 13:09:28 -0700

    Styles homepage

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,8 +5,11 @@ function Welcome() {
     <div className="homepage">
       <div className="welcome">
         <h2>Welcome to Nom Nom Plants!</h2>
-        <p>Nom Nom Plants is a place for users to track how many different 
-          plants they eat per week by growing virtual gardens! Recent research
+        <p className="first-line">Nom Nom Plants is a place for users to track how many different 
+          plants they eat per week by growing virtual gardens. 
+        </p>
+        <p>  
+          Recent research
           suggests that consuming at least 30 different plant varieties per week
           supports a healthy gut microbiome, which is itself associated with greater
           health and wellbeing. Diversifying your plant diet can be a fun and colorful way
@@ -15,7 +18,11 @@ function Welcome() {
           <p>
             If 30 different plants sounds like a lot, don't fret! "Plants" applies to much more than just
             fruits and vegetables. Think whole grains, nuts, seeds, legumes, herbs, and spices as well! For inspiration, click the 
-            Demo button to see Lovebug's weekly gardens, and all the different plants she likes to eat. Through the demo, you can also
+            Demo button to see Lovebug's weekly gardens, and all the different plants she likes to eat. 
+          </p>
+          <p>
+            
+            Through the demo, you can also
             explore the app's features. Click the "back" and "forward" leaf buttons at the top of the page to view different weeks, and click on 
             individual plants to edit or delete them. Add new plants by clicking the + icon. The chart button lets you visualize 
             your gardening progress.
