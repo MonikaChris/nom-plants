abee9441746db71bc16377b58dc08f2e9805c879
Monika <monidavies@gmail.com>
2023-11-30 10:10:36 -0800

    Adds test for login input

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Login from '../components/Login.js';
 import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
 const Wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>
 
@@ -11,7 +12,20 @@ describe('Login Component', () => {
     expect(screen.getByLabelText(/username/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /sign in/i})).toBeInTheDocument();
-  })
+  });
+
+  test('user can enter username and password', async () => {
+    render(<Login />, { wrapper: Wrapper });
+
+    const usernameInput = screen.getByLabelText(/username/i);
+    const passwordInput = screen.getByLabelText(/password/i);
+
+    await userEvent.type(usernameInput, 'testuser');
+    await userEvent.type(passwordInput, 'testpassword');
+
+    expect(usernameInput.value).toBe('testuser');
+    expect(passwordInput.value).toBe('testpassword');
+  });
 })
 
 
