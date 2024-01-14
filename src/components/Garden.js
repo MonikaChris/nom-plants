c91c8e8a5fe48ec99f25f200a0728b658bb1b5a2
Monika <monidavies@gmail.com>
2024-01-14 10:37:22 -0800

    Refactors getPlantRows

--- src/components/Garden.js
+++ src/components/Garden.js
@@ -7,11 +7,13 @@ function Garden({ plants, GARDEN_STARTING_SIZE, GARDEN_ROW_LENGTH, setEditPlantM
   
   function getPlantRows() {
     //Shapes garden array and fills in 0's to represent seedbeds
-    let gardenArray = new Array(GARDEN_STARTING_SIZE).fill(0);
-    plants.forEach((plant, idx) => gardenArray[idx] = plant);
+    let gardenArray = [];
+    plants.forEach((plant) => gardenArray.push(plant));
+    while (gardenArray.length < GARDEN_STARTING_SIZE) gardenArray.push(0);
+
     if (plants.length > GARDEN_STARTING_SIZE) {
       const num_zeros = GARDEN_ROW_LENGTH - ((plants.length - GARDEN_STARTING_SIZE) % GARDEN_ROW_LENGTH);
-      gardenArray = gardenArray.concat(new Array(num_zeros).fill(0));
+      if (num_zeros < GARDEN_ROW_LENGTH) gardenArray = gardenArray.concat(new Array(num_zeros).fill(0));
     }
     return groupRows(gardenArray);
   }
